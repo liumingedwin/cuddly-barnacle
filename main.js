228eb28ac951c7af2fe70b49c7c67a9fdecb7734
Edwin Lau <liumingedwin@163.com>
2025-02-05 15:07:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const https = require("https");
 const fs = require("fs");
-const m  = [`http://mckuhei.mcmodule.org/huge_2023-06-06_11.19.41.png huge`];
+const m  = [`http://mckuhei.mcmodule.org/huge_2023-06-06_11.19.41.png huge-2023`];
 for (let i = 0; i < m.length; ++i){
     let u = m[i].split(" ")[0]; 
     let n = m[i].split(" ")[1]; 
